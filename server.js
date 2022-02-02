2c0cc8da52b67648ab0c51d9e292e6316420dc8a
Santu Koley <santu.koley@ingreens.in>
2022-02-02 19:58:41 +0530

    initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+// setTimeout(()=>{
+//   app.listen(PORT, HOST);
+//   console.log("App Started");
+// }, 150000)
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
